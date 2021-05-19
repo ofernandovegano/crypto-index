57141672fadf53a928cb5e2bf2e5efec14fc67b3
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-05-19 01:06:04 -0300

    added homepage route and html structure

--- app/javascript/react-app/App.js
+++ app/javascript/react-app/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
+import Homepage from './pages/homepage/homepage.component';
 import Login from './components/login/login.component';
 
 
 const App = () => (
   <div className="App">
-    <Login />
+    <Switch>
+      <Route exact path='/' component={Homepage} />
+      <Route exact path="/login" component={Login} />
+    </Switch>
   </div>
 );
 
